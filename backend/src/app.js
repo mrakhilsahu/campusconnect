133bb13066cd15a25e27c4383ca3645570cc9a91
akhil sahu <akhilsahu925@gmail.com>
2026-03-20 00:12:03 +0530

    Updated app setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const eventRoutes = require("./routes/eventRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend URL
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+
+app.use("/api/test", require("./routes/testRoutes"));
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/events", eventRoutes);
+app.use("/api/registrations", require("./routes/registrations"));
+
 
 app.get("/", (req, res) => {
   res.send("CampusConnect API running");
