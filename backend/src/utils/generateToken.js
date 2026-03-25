ff4d616c085bfaf1509da97e1da602aacba9bbbe
akhil sahu <akhilsahu925@gmail.com>
2026-03-25 23:46:06 +0530

    Added utility functions

---
+++ backend/src/utils/generateToken.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (user) => {
+  return jwt.sign(
+    {
+      userId: user._id,
+      role: user.role,
+      collegeId: user.collegeId,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+};
+
+module.exports = generateToken;
